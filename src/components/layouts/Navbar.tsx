67f3cff7bfddfd25a119b31dbf77520d111da1dd
telkom.perizinan.y <yanuardi48@gmail.com>
2023-07-21 13:24:11 +0700

    isActived menu

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -7,19 +7,44 @@ const Navbar = () => {
   const navigateTo = (path: string) => {
     navigate(path);
   };
+
+  const isActivedMenu = (path: string) => {
+    if (path === location.pathname)
+      return `border-b-[3px] border-[#FFFF00] font-bold`;
+    else return ``;
+  };
+
   return (
     <nav className="w-full bg-[#0E0E0E] py-[18px] px-[48px] flex items-center justify-between">
       <img src={LogoMaxima} alt="logo-maxima" />
       <div className="flex gap-20">
-        <button onClick={() => navigateTo('/')} className="text-[20px] leading-[30px] tracking-[-0.015em] font-bold pb-[4px]">
-          <span className={`${location.pathname === '/' ? 'border-b-[3px] border-[#FFFF00]' : ''}`}>Home</span>
+        <button
+          onClick={() => navigateTo("/")}
+          className="text-[20px] leading-[30px] tracking-[-0.015em] font-normal pb-[4px]"
+        >
+          <span className={isActivedMenu("/")}>Home</span>
         </button>
-        <button onClick={() => navigateTo('/about')} className="text-[20px] leading-[30px] tracking-[-0.015em] font-normal pb-[4px]"><span className={`${location.pathname === '/about' ? 'border-b-[3px] border-[#FFFF00]' : ''}`}>About</span></button>
-        <button onClick={() => navigateTo('/work')} className="text-[20px] leading-[30px] tracking-[-0.015em] font-normal pb-[4px]"><span className={`${location.pathname === '/work' ? 'border-b-[3px] border-[#FFFF00]' : ''}`}>Work</span></button>
-        <button onClick={() => navigateTo('/contact')} className="text-[20px] leading-[30px] tracking-[-0.015em] font-semibold py-[5px] px-[20px] transition ease-in-out rounded-full border border-white hover:border-[#FFFF00] hover:text-black hover:bg-[#FFFF00] hover:duration-700">Contact Us</button>
+        <button
+          onClick={() => navigateTo("/about")}
+          className="text-[20px] leading-[30px] tracking-[-0.015em] font-normal pb-[4px]"
+        >
+          <span className={isActivedMenu("/about")}>About</span>
+        </button>
+        <button
+          onClick={() => navigateTo("/work")}
+          className="text-[20px] leading-[30px] tracking-[-0.015em] font-normal pb-[4px]"
+        >
+          <span className={isActivedMenu("/work")}>Work</span>
+        </button>
+        <button
+          onClick={() => navigateTo("/contact")}
+          className="text-[20px] leading-[30px] tracking-[-0.015em] font-semibold py-[5px] px-[20px] transition ease-in-out rounded-full border border-white hover:border-[#FFFF00] hover:text-black hover:bg-[#FFFF00] hover:duration-700"
+        >
+          Contact Us
+        </button>
       </div>
     </nav>
-  )
+  );
 };
 
 export default Navbar;
