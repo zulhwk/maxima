e8c914eb1b9ddd34c502f43db0c70197ce11c3fd
telkom.perizinan.y <yanuardi48@gmail.com>
2023-08-17 09:28:50 +0700

    GA: pathname dynamics useeffect

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,9 @@ import ReactGA from 'react-ga';
 function App() {
   useEffect(() => {
     ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
+    console.log(window.location.pathname + window.location.search, "HELOO WORLDS");
+  }, [window.location.pathname]);
+
   const routes = useRoutes(allRoutes());
   return routes;
 }
